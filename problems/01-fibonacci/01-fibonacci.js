79d3ae90c4a3ce4813b59b22ff43476710d38064
Wilfred Perez <wperez@aol.com>
2019-11-14 23:43:32 -0500

    completed Assign 1 and 2

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,14 @@
-/* 1. Fibonacci
-Define function: fib(n) 
-Return the nth number in the fibonacci sequence. */
+//prob 1. Fibonacci
 function fib(n) {
-
+    let o = 0
+    let p = 1 
+    let temp = 0
+    //iterate (n to 1), adding.
+    for (let i=n; i>0 ; i--) {
+        temp = o
+        o = o + p
+        p = temp
+    }
+    return o
 }
+console.log(`The nth number is equal to fib= ${fib(6)}`)
--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -1,7 +1,24 @@
-/* 2. Bubble Sort
-Define function: bubbleSort(numArray)
-Use the bubble sort algorithm to sort the array.
-Return the sorted array. */
-function bubbleSort(numArray) {
-  
+// 2. Bubble Sort
+function bubbleSort(numArray)
+{
+    let swapped
+    let arr = numArray
+    n = numArray.length -1
+    do {
+        swapped = false
+        for (let i=0; i < n; i++)
+        {
+            if (arr[i] < arr[i+1])
+            {
+               let temp = arr[i]
+               arr[i] = arr[i+1]
+               arr[i+1] = temp
+               swapped = true
+            }
+        }
+        n--
+    } while (swapped)
+ return arr 
 }
+numArray = (bubbleSort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]))
+console.log(bubbleSort(numArray))
