fc4dd4bd64f8d6ee218fa76cbabd7b555edeec77
Wilfred Perez <wperez@aol.com>
2019-11-16 18:19:27 -0500

    Assign 1 thru 7 completed

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,10 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
-}
+  if ((!(someNum & 1 ))){ 
+      return true
+    } else{
+        return false
+    }
+} 
+console.log(isEven(40))
--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,12 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
-function isPalindrome(someStr) {
-  
-}
+    function isPalindrome(someStr) {     
+        let reverse = ""
+        //let someStr = ""        		            
+        for (let i = someStr.length-1; i >= 0 ; i--) {       
+                reverse += someStr.charAt(i)               
+        }
+        return ((reverse === someStr) ? true : false)
+   }
+console.log(isPalindrome("racecar"))
